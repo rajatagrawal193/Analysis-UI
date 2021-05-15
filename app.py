98ecfbb8ba47dabeafa4538fe443b212e9b88c8e
Rajat Agrawal <rajat.agrawal193@gmail.com>
2021-05-16 04:03:01 +0530

    Added Work time graphs tab, monthly weekly graphs, optimised code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from dashboards.sleep_pattern import SleepPattern
 from dashboards.workout_events import WorkoutGraphs
 from dashboards.mess_graphs import MessGraphs
+from dashboards.worktime_graphs import WorktimeGraphs
 from layout import get_layout
 import dash
 import dash_html_components as html
@@ -36,6 +37,7 @@ default_end_date = (date.today() + timedelta(days=1)).isoformat()
 workout_graphs = WorkoutGraphs(default_start_date, default_end_date)
 sleep_pattern = SleepPattern(default_start_date, default_end_date )
 mess_graphs = MessGraphs(default_start_date, default_end_date)
+worktime_graphs = WorktimeGraphs(default_start_date, default_end_date)
 
 
 @app.callback(dash.dependencies.Output('page-content', 'children'),
@@ -53,8 +55,10 @@ def display_dashboard(pathname, session_id):
     #     return major_events.content
     elif pathname == '/sleep_pattern':
         return sleep_pattern.content
-    elif pathname == '/workout_events':
+    elif pathname == '/workout_graphs':
         return workout_graphs.content
+    elif pathname == '/worktime_graphs':
+        return worktime_graphs.content
     else:
         return html.Div([
             html.Div('You are on page {}'.format(pathname))
@@ -83,6 +87,19 @@ def update_workout_events_graphs(start_date, end_date):
     wg = WorkoutGraphs(start_date, end_date)
 
     return wg.get_workout_events_graph(), wg.get_weekly_workout_graph(), wg.get_monthly_workout_graph()
+
+
+@app.callback(
+    [Output(WorktimeGraphs.WORKTIME_EVENTS_GRAPH_ID, 'figure'),
+     Output(WorktimeGraphs.WEEKLY_WORKTIME_EVENTS_GRAPH_ID, 'figure'),
+     Output(WorktimeGraphs.MONTHLY_WORKTIME_EVENTS_GRAPH_ID, 'figure'),],
+    [Input(component_id='date-picker', component_property='start_date'),
+        Input(component_id='date-picker', component_property='end_date'),]
+)
+def update_workout_events_graphs(start_date, end_date):
+    wg = WorktimeGraphs(start_date, end_date)
+
+    return wg.get_worktime_events_graph(), wg.get_weekly_worktime_graph(), wg.get_monthly_worktime_graph()
 
 
 @app.callback(
--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -6,7 +6,7 @@ from datetime import date
 def get_events_by_week(events: [CalendarEvent]) -> dict[str, [CalendarEvent]]:
     weekly_segregated_events = {}
     for event in events:
-        year, week_no, week_day_no = event.start.date().isocalendar()
+        year, week_no, week_day_no = event.date.isocalendar()
         week_start = date.fromisocalendar(year, week_no, 1).strftime("%b %d")
         week_end = date.fromisocalendar(year, week_no, 7).strftime("%b %d")
 
@@ -23,7 +23,7 @@ def get_events_by_week(events: [CalendarEvent]) -> dict[str, [CalendarEvent]]:
 def get_events_by_month(events):
     events_by_month = {}
     for event in events:
-        month = str(event.start.date().strftime("%b '%y"))
+        month = str(event.date.strftime("%b '%y"))
         if month not in events_by_month:
             events_by_month[month] = [event]
         else:
@@ -31,15 +31,44 @@ def get_events_by_month(events):
     return events_by_month
 
 
-def generate_weekly_frequency_graph(title, calendar_events, start_date, end_date) -> go.Figure():
+MEASURE_DURATION = "DURATION"
+MEASURE_COUNT = "COUNT"
+INTERVAL_DAILY = "Daily"
+INTERVAL_WEEKLY = "Weekly"
+INTERVAL_MONTHLY = "Monthly"
+
+#
+# def generate_daily_frequency_graph(title, calendar_events, start_date, end_date, measure, night_shift=None) -> go.Figure():
+#
+#     graph = Graph(f"Daily {title} Events | Avg: {avg} ", {measure}, "Day")
+#     for event in calendar_events:
+
+
+def generate_weekly_monthly_trends(title, calendar_events, start_date, end_date, interval, duration=False) -> go.Figure():
     delta = end_date - start_date
     total_days = delta.days
     avg = len(calendar_events) / total_days * 7.0
-    graph = Graph(f"Weekly {title} Events | Avg: {avg} ", "Frequency", "Week No")
-    weekly_segregated_events = get_events_by_week(calendar_events)
-    for week, events in weekly_segregated_events.items():
-        graph.x.append(week)
-        graph.y.append(len(events))
+    measure = MEASURE_DURATION if duration else MEASURE_COUNT
+    graph = Graph(f"{interval} {title} Events | Avg: {avg} ", {measure}, interval)
+    segregated_events = None
+    if interval == INTERVAL_WEEKLY:
+        segregated_events = get_events_by_week(calendar_events)
+    if interval == INTERVAL_MONTHLY:
+        segregated_events = get_events_by_month(calendar_events)
+    if segregated_events:
+        for interval_name, events in segregated_events.items():
+            graph.x.append(interval_name)
+            if duration:
+                total_duration = 0
+                for event in events:
+                    total_duration += event.duration_in_hours
+                graph.y.append(total_duration)
+            else:
+                graph.y.append(len(events))
+    else:
+        for event in calendar_events:
+            graph.x.append(event.date)
+            graph.y.append(event.duration_in_hours)
 
     fig = go.Figure(data=[go.Bar(x=graph.x, y=graph.y, name=graph.title)], layout=dict(title=dict(text=graph.title)))
     # fig = graph.update_fig_layout(fig)
